a00731fe0e134d37f382cdf408c60a2e960d0c21
darkpaw <darkpaw@internode.on.net>
2016-04-09 15:35:51 +1000

    plot some demand vs generation

--- load_data.py
+++ load_data.py
@@ -3,15 +3,27 @@ import pandas as pd
 
 def load_combined():
 
-    gen_normalised_df = pd.read_csv('model_data/generation_half_hourly.txt', names=['Generation'], header=None)
-    #print(gen_normalised_df)
+    base_df = pd.DataFrame({'Time': range(48)})
+    generation = [base_df]
 
-    test_df = pd.read_csv('model_data/generation_half_hourly.txt', names=['Consumption'], header=None)
-    #print(test_df)
+    for i in range(6):
+        gen_normalised_df = pd.read_csv('model_data/gen_profile_%d.txt' % i, names=['Gen_%d' % i], header=None)
+        generation.append(gen_normalised_df)
 
-    combined = pd.concat([gen_normalised_df, test_df], axis=1)
-    print(combined)
+    load = []
+    for idx, letter in enumerate(['A', 'B', 'C', 'D', 'E', 'F']):
+        load_df = pd.read_csv('model_data/Person%s.csv' % letter, names=['Load_%d' % idx], header=None)
+        load.append(load_df)
+
+    generation.extend(load)
+    combined = pd.concat(generation, axis=1)
+
+    #print(combined)
 
     return combined
 
 
+if __name__ == "__main__":
+    df = load_combined()
+    assert isinstance(df, pd.DataFrame)
+    df.to_csv("model_data/generation_and_load_half_hourly.csv")
