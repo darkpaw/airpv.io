c1db88468d0ecd04630f566fae575cd80a4d542f
darkpaw <darkpaw@internode.on.net>
2016-04-09 16:54:34 +1000

    daily generation plots

--- plot_multi.py
+++ plot_multi.py
@@ -8,29 +8,16 @@ import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 plt.style.use('fivethirtyeight')
 
-# plt.plot([1,2,3,4])
-# plt.ylabel('some numbers')
-# plt.show()
-
 df = load_combined()
 assert isinstance(df, pd.DataFrame)
-#gen = df['Gen_1']
 
 people = (df['Load_0'] + df['Load_1'] + df['Load_3']) * 5.0
-print(people)
 
-plt.plot(df['Gen_4'] * 10.0)
-#plt.plot(df['Gen_0'] * 10.332)
-#plt.plot(people)
-
-plt.plot(df['Load_3'] * 5)
-
-# plt.plot(df['Load_3'] * 5)
-# plt.plot(df['Gen_3'])
-# plt.plot(df['Gen_4'])
-# plt.plot(df['Gen_5'])
-
-plt.ylabel('Generation kW')
-plt.xlabel('1 day - 48 half hours')
-plt.show()
-
+for gen in range(6):
+    plt.plot(df['Gen_%d' % gen] * 10.0)
+    plt.plot(people)
+    plt.ylabel('Generation kW')
+    plt.xlabel('1 day - 48 half hours')
+    plt.tight_layout()
+    plt.savefig('day_%d.png' % gen)
+    plt.close()
